7aff46e3995dd946242bdef5aba3e623b0ecc03a
Van-Shuimu <81547797+Van-Shuimu@users.noreply.github.com>
2021-07-01 09:57:47 +0300

    New scene
    Made another scene

---
+++ Assets/Scripts/RemoveObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveObject : MonoBehaviour
+{
+    public void Remove()
+    {
+        Destroy(gameObject);
+    }
+}
